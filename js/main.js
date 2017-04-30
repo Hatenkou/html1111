251a99f07607606a7c7c8fbf94bd59e45e090dc0
Victor <vitya199600@mail.ru>
2017-04-30 13:40:24 +0000

    navbar

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
+$(function() {
+  $(document).on('click','.navbar-collapse.in',function(e) {
+      if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
+          $(this).collapse('hide');
+      }
+  });
+});
+$(function() {
+    $('.smooth').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 2000);
+        }
+    });
+});
+
 (function (window, $) {
 	'use strict';
 
